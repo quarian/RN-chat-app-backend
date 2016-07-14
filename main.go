45872057ffc361b8ca44c8fabc44a2a627d78e24
Miro Nurmela <mnur@fl899.local>
2016-07-14 15:08:47 +0300

    Spouts out chuck norris jokes

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"os"
   "strconv"
+	"io/ioutil"
+	"encoding/json"
 
 	"github.com/gin-gonic/gin"
 	"github.com/russross/blackfriday"
@@ -15,6 +17,15 @@ var (
 	repeat int
 )
 
+type Joke struct {
+	Type string `json:"type"`
+	Value struct {
+		Id int `json:"id"`
+		Joke string `json:"joke"`
+		Categories []string `json:"categories"`
+	} `json:"value"`
+}
+
 
 func repeatHandler(c *gin.Context) {
 	var buffer bytes.Buffer
@@ -23,6 +34,60 @@ func repeatHandler(c *gin.Context) {
     	}
     	c.String(http.StatusOK, buffer.String())
 }
+
+func quoteHandler(c *gin.Context) {
+	quoteBody, err := getNorrisQuoteBody("http://api.icndb.com/jokes/random")
+	if err != nil {
+		c.String(http.StatusOK, err.Error())
+	} else {
+		joke, err := getJoke(quoteBody)
+		if err != nil {
+			c.String(http.StatusOK, err.Error())
+		} else {
+			c.String(http.StatusOK, joke)
+		}
+	}
+}
+
+func getJoke(quoteBody []byte) (string, error) {
+	var joke Joke
+	err := json.Unmarshal(quoteBody, &joke)
+	log.Println(joke)
+	if err != nil {
+		return "", err
+	}
+	return joke.Value.Joke, err
+
+}
+
+func getNorrisQuoteBody(url string) ([]byte, error) {
+	quote, err := queryQuoteAPI(url)
+	return quote, err
+}
+
+func queryQuoteAPI(url string) ([]byte, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	client :=&http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	queryBody, err := readResponse(resp)
+	return queryBody, err
+}
+
+func readResponse(resp *http.Response) ([]byte, error) {
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, err
+}
+
 
 func main() {
   var err error
@@ -52,6 +117,8 @@ func main() {
 
 
   router.GET("/repeat", repeatHandler)
+
+	router.GET("/quote", quoteHandler)
 
 	router.Run(":" + port)
 }
